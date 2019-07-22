ae5a451c0b7dbfc1df12c184f7d8b463426f75e0
janvda <jan@Jans-MacBook-Pro.local>
2019-07-22 18:09:40 +0200

    prefixes the mount options with -o if specified

--- samba-rsync/start.sh
+++ samba-rsync/start.sh
@@ -6,9 +6,14 @@ if [ "$smb1_server_and_share_name" != '' ]; then
    fi
    export smb1_mount_point=/data/from/$smb1_mount_folder
 
+   # prefix the mount options with -o if specified
+   if [ "$smb1_mount_options" != '' ]; then
+      export smb1_full_mount_options= "-o $smb1_mount_options"
+   fi
+
    echo "Mounting samba share 1"
    mkdir -p $smb1_mount_point
-   mount -t cifs $smb1_mount_options $smb1_server_and_share_name $smb1_mount_point
+   mount -t cifs $smb1_full_mount_options $smb1_server_and_share_name $smb1_mount_point
 fi
 
 sleep 3600
